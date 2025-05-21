329041eec3d5a3301c35043fdf88051325754345
Spring -Git <vaahne@yahoo.com>
2025-05-21 15:54:46 -0600

    home styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import styles from './HomePage.module.css';
 
 export default function HomePage(){
     const nav = useNavigate();
@@ -13,8 +14,18 @@ export default function HomePage(){
         }else
             nav(`/category`);
     }
-    return <div className="homeContent">
-        <button onClick={handleClick} name="start">Start Quiz</button>
-        <button onClick={handleClick} name="random">Random Quiz</button>
+    return <div className={styles.homeContent}>
+        <div className={styles.leftContent}>
+            <img src="https://media.tenor.com/1dKzIzThtzgAAAAM/trivia-day-happy-trivia-day.gif" />
+            <img src="https://media.tenor.com/xyvcwiSy0V0AAAAM/trivia-trivia-time.gif"/>
+        </div>
+        <div className={styles.homeButtons}>
+            <button onClick={handleClick} name="start">Start Quiz</button>
+            <button onClick={handleClick} name="random">Random Quiz</button>
+        </div>
+        <div className={styles.rightContent}>
+            <img src="https://media.tenor.com/1dKzIzThtzgAAAAM/trivia-day-happy-trivia-day.gif" />
+            <img src="https://media.tenor.com/xyvcwiSy0V0AAAAM/trivia-trivia-time.gif" />
+        </div>
     </div>  
 }
