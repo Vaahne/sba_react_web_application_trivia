31af2638ceb536dbf0cbb62e3a510b2424088aeb
Spring -Git <vaahne@yahoo.com>
2025-05-21 21:35:50 -0600

    nav transitions

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,9 +6,9 @@ export default function NavBar(){
     return <nav className={styles.navBar}> 
             <ul>
                 <li><img className={styles.logo} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1MBqEotsRLa9WLXDrxfC4T8gaU0tLDRAkMg&s'/></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/category">Categories</Link></li>
-                <li><Link to="/aboutUs">About Us</Link>  </li>
+                <li><Link to="/" className={styles.navItems}>Home</Link></li>
+                <li><Link to="/category" className={styles.navItems}>Categories</Link></li>
+                <li><Link to="/aboutUs" className={styles.navItems}>About Us</Link>  </li>
             </ul>
         </nav>
 }
